fa23ea229a5f8d76046adb2ba44d164169fc60f3
Mike Loranty <mloranty@colgate.edu>
2021-07-30 14:47:58 -0400

    Added code to combine data files and calculate some basic summary stats for AGU abstract.

--- data_cleaning.R
+++ data_cleaning.R
@@ -23,8 +23,8 @@ f <- list.files(path = "pcn_fire_synthesis_data/csv/",
                 pattern = ".csv", full.names = TRUE)
 
 
-#### CLEAN UP INDIVIDUAL FILES ####
-
+#### CLEAN UP AND CONCATENATE INDIVIDUAL FILES ----
+##############
 ## Baillargeon
 f1 <- read.csv(f[1], header = TRUE) 
 
@@ -40,14 +40,9 @@ f1$fire_year <- as.numeric(sub("unburned", NA, f1$fire_year, fixed = TRUE))
 # fix slope, which was read as logical
 f1$slope <- read.csv(f[1], header = TRUE, colClasses = "character")[,20]
 
-
-## Breen 
-f2 <- read.csv(f[2], header = TRUE)
-
-# fix thaw/active column, which was read as logical
-f2$thaw_active <- read.csv(f[2], header = TRUE, colClasses = "character")[,24]
-
-
+# fix thaw_active - these are TD measurements, not ALD
+f1$thaw_active <- "A"
+##############
 ## Buma
 # read without 17th column, which is redundant to the gt_prob column
 f3 <- read.csv(f[3], header = TRUE)[,-17]
@@ -55,9 +50,12 @@ f3 <- read.csv(f[3], header = TRUE)[,-17]
 # fix thaw/active column, which was read as logical
 f3$thaw_active <- read.csv(f[3], header = TRUE, colClasses = "character")[,20]
 
-# aggregate by site and treatment (see Notes from Buma)
+# fix site_id for aggregating (see Note below from Buma)
+f3$site_id <- paste(f3$site_id, sapply(strsplit(f3$plot,"_"),"[[",2), sep="_" )
 
+# The Dalton and the Steese are larger "sites," each of which has a lot of plots in them.  But those plots do vary, so don't aggregate.  At the Dalton, there are sites with 0 (unburned), 1 (one fire, which would be 2004/2005 era), 2 fires (which would be 1970's era AND 2004 or 2005), or 3 fires (which would be 1950's, 1970's, and 2004 or 2005.  So if aggregating, what you'd want to do would be to aggregate by those treatments (0, 1, 2, or 3 fires) within the Dalton or Steese "sites."  So, sounds like you'd want to aggregate all the unburned plots at the Dalton, all the 1 burn plots at the Dalton, etc.  I would not aggregate Dalton and Steese plots together, they are functionally different sites (uplands vs. low lands, respectively).
 
+##############
 ## Dielman
 f4 <- read.csv(f[4], header = TRUE)
 
@@ -104,26 +102,35 @@ f9$organic_depth <- as.numeric(f9$organic_depth)
 # fix incorrect logical columns
 f9[,c(20:22)] <- read.csv(f[9], header = TRUE, colClasses = "character")[,c(20:22)]
 
+# remove characters from thaw depth and convert to numeric
+f9$thaw_depth <- gsub("+", "", f9$thaw_depth, fixed = TRUE)
+f9$thaw_depth <- as.numeric(gsub(">", "", f9$thaw_depth, fixed = TRUE))
+
 
 ## O'Donnell
 f10 <- read.csv(f[10], header = TRUE)
 
 # remove non-numeric characters from numeric vars
-# note we're loosing info on where old is in excess of the entered value
+# note we're loosing info on where Organic Layer Thickness is in excess of the entered value
 f10$organic_depth <- as.numeric(gsub(">", "",f10$organic_depth))
 
 f10$thaw_depth <- as.numeric(gsub(">", "",f10$thaw_depth))
 
 
-## Gibson
+## Gibson/Olefeldt
 f11 <- read.csv(f[11], header = TRUE)
 
 # fix incorrect logical columns
 f11$slope <- read.csv(f[11], header = TRUE, colClasses = "character")[,20]
 
+# remove non-numeric characters from numeric vars
+# note we're loosing info on where Organic Layer Thickness is in excess of the entered value
+f11$organic_depth <- as.numeric(gsub(">", "",f11$organic_depth))
+
 
 ## Paulson
 f12 <- read.csv(f[12], header = TRUE)
+
 
 # fix incorrect logical columns
 f12$thaw_active <- read.csv(f[12], header = TRUE, colClasses = "character")[,19]
@@ -140,3 +147,44 @@ f13$organic_depth <- as.numeric(f13$organic_depth)
 
 # convert thaw depth to numeric - blank cells have a period, and will be converted to NA
 f13$thaw_depth <- as.numeric(f13$thaw_depth)
+
+#### concatenate and clean up all of the raw data -----
+all.td <- rbind(f1,f3,f4,f5,f6,f7[,-23],f8[,-23],f9, f10, f11, f12[,-23], f13[,-23])
+
+### fix inconsistent spelling/capitalization for burned/unburned
+all.td$burn_unburn[grep("unb", all.td$burn_unburn, ignore.case = TRUE)] <- "unburned"
+all.td$burn_unburn[grep("unb", all.td$burn_unburn, ignore.case = TRUE, invert = TRUE)] <- "burned"
+
+### fix inconsistent spelling/capitalization for boreal/tundra
+all.td$boreal_tundra <- sub("B", "boreal", all.td$boreal_tundra)
+all.td$boreal_tundra <- sub("T", "tundra", all.td$boreal_tundra)
+
+# convert day to numeric
+all.td$day <- as.numeric(all.td$day)
+
+# calculate time since fire
+all.td$tsf <- all.td$year-all.td$fire_year
+
+# set time since fire to 200 for unburned 
+all.td$tsf[which(all.td$burn_unburn=="unburned")] <- 200
+
+
+all.td$thaw_active[which(all.td$thaw_active=="TRUE")] <- "T"
+########### Files submitted pre-aggregated to the site level ----
+## Breen 
+f2 <- read.csv(f[2], header = TRUE)
+
+# fix thaw/active column, which was read as logical
+f2$thaw_active <- read.csv(f[2], header = TRUE, colClasses = "character")[,24]
+
+
+### AGU Analyses ----
+#------------------------------------------------------------------------------------------#
+## calculate number of TD measurements for AGU abstract
+nrow(f1)+nrow(f2)+nrow(f3)+nrow(f4)+nrow(f5)+nrow(f6)+nrow(f7)+nrow(f8)+nrow(f9)+nrow(f10)+nrow(f11)+nrow(f12)+nrow(f13)
+
+aggregate(cbind(thaw_depth,tsf)~boreal_tundra+burn_unburn, all.td, FUN = mean)
+aggregate(cbind(thaw_depth,tsf)~boreal_tundra+burn_unburn+thaw_active, all.td, FUN = mean)
+
+site.td <- aggregate(cbind(thaw_depth,tsf)~last_name+site_id+year+month+burn_unburn,all.td, FUN=mean)
+
